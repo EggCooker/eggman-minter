34719b433f4567a2a4390fc555f63ef63ee7e2b2
EggCooker <128488008+EggCooker@users.noreply.github.com>
2025-06-26 16:17:21 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is the Eggman Mini App. Try /frame.");
+});
+
+app.get("/frame", (req, res) => {
+  res.json({
+    version: "vNext",
+    image: "https://i.imgur.com/Qr71crq.jpeg",
+    buttons: [
+      {
+        label: "Mint Eggman NFT",
+        action: "post",
+        target: "https://your-app-name.vercel.app/mint"
+      }
+    ]
+  });
+});
+
+app.post("/mint", (req, res) => {
+  res.json({
+    version: "vNext",
+    image: "https://i.imgur.com/EB8Y63k.jpeg",
+    text: "Mint complete!",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`? Mini App running at http://localhost:${port}/frame`);
+});
