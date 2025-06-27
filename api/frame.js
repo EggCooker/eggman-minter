cb68d7386dc1792cfbc171556ad3e6660cca88f1
EggCooker <128488008+EggCooker@users.noreply.github.com>
2025-06-27 17:23:14 +0900

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,16 +1,17 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json({
-    version: "vNext",
-    image: "https://placekitten.com/600/400",
-    text: "Mint Eggman",
-    buttons: [
-      {
-        label: "Mint",
-        action: "post",
-        target: "https://eggman-minter.vercel.app/api/mint"
-      }
-    ]
-  });
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      version: "vNext",
+      image: "https://placekitten.com/600/400",
+      text: "Mint Eggman",
+      buttons: [
+        {
+          label: "Mint",
+          action: "post",
+          target: "https://eggman-minter.vercel.app/api/mint"
+        }
+      ]
+    })
+  );
 }
-
